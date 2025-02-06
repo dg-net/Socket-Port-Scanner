4ccccf78040707012a23408013a83cab121029ec
Bird <bird@localhost.localdomain>
2025-02-06 13:14:43 +0900

    feat: Add libraries to build the script

---
+++ scanner.py
@@ -0,0 +1,6 @@
+#Library for executing multipleinstances of a function as once
+#Does simultanious exection with threading
+from concurrent.futures import ThreadPoolExecutor
+#Allows connections between ports with TCP
+import sockets
+import time

3c118703c45ee2a778b7dfc5bf4d508ad82c4fee
Bird <bird@localhost.localdomain>
2025-02-06 13:51:07 +0900

    feat: Add function to get valid ip address from the user and call itself if ip address is invalid

--- scanner.py
+++ scanner.py
@@ -1,6 +1,22 @@
-#Library for executing multipleinstances of a function as once
+#Library for executing multiple instances of a function as once
 #Does simultanious exection with threading
 from concurrent.futures import ThreadPoolExecutor
 #Allows connections between ports with TCP
-import sockets
+import socket
+#Library to check if user inputted ip address is a valid ip address
+from ipaddress import ip_address
 import time
+
+def get_ip():
+    ip = input("\nPlease enter ip address: ")
+    try:
+        ip_address(ip)
+        return ip
+    except ValueError:
+        print("\nInvalid ip address")
+        return get_ip()
+
+if __name__ == "__main__":
+    ip = get_ip()
+    print(ip)
+
